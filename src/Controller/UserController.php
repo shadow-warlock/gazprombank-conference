19a15b868985c578477d8637c11e4f190a6e407c
timager <timger98@gmail.com>
2020-05-14 17:17:46 +0300

    user delete

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,12 +6,28 @@ use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
 class UserController extends AbstractController {
+    /**
+     * @Route("/api/user/{id}", name="user_delete", methods={"DELETE"})
+     * @param $id
+     * @return JsonResponse
+     */
+    public function deleteUser($id) {
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = $entityManager->find(User::class, $id);
+        if(!$user) {
+            throw new NotFoundHttpException("user not found");
+        }
+        $entityManager->remove($user);
+        $entityManager->flush();
+        return $this->json(true, 204);
+    }
 
     /**
      * @Route("/api/user", name="user_add", methods={"POST"})
