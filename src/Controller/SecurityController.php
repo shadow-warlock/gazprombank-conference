8fa4d67d7b828e8882fe8fd1bb31236057fbaf04
timager <timger98@gmail.com>
2020-05-16 17:57:27 +0300

    security update

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,13 +7,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController {
-
     /**
      * @Route("/api/session", name="user_login", methods={"POST"})
      */
     public function userLogin() {
         $user = $this->getUser();
         return $this->json($user);
+    }
+
+    /**
+     * @Route("/api/session", name="get_logged_user", methods={"GET"})
+     */
+    public function getLoggedUser(){
+        $this->denyAccessUnlessGranted(User::IS_AUTHENTICATED_FULLY);
+        return $this->json($this->getUser());
     }
 
     /**
