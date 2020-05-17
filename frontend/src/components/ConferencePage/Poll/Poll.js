b69f7394cd84cb6e10fda41b1ae9a5adb4120746
timager <timger98@gmail.com>
2020-05-18 02:57:15 +0300

    Polls style

--- frontend/src/components/ConferencePage/Poll/Poll.js
+++ frontend/src/components/ConferencePage/Poll/Poll.js
@@ -1,11 +1,31 @@
 import React, {Component} from "react";
+import "./Poll.css";
+import Button from "../../Button/Button";
+import Input from "../../Input/Input";
 
 export default class Poll extends Component {
+
+    getVariants() {
+        if (this.props.poll.variants) {
+            return this.props.poll.variants.map((variant) => <Button>{variant}</Button>);
+        } else {
+            return (
+                <>
+                    <Input placeholder={"Ваш ответ"}/>
+                    <Button>Ответить</Button>
+                </>
+            )
+        }
+    }
+
     render() {
         return (
-            <div>
-                <p>{this.props.poll.question}</p>
-                <input/>
+            <div className={"poll"}>
+                <p className={"color_white font_size_very_big uppercase"}>Опрос</p>
+                <p className={"color_pink font_size_big uppercase bold"}>{this.props.poll.question}</p>
+                <div className={"variants"}>
+                    {this.getVariants()}
+                </div>
             </div>
         );
     }
