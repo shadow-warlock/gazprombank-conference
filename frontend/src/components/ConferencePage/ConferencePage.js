6e09df681d5e6a727cdcfa27628174e39ff9e2d9
timager <timger98@gmail.com>
2020-05-18 23:15:33 +0300

    style fixes

--- frontend/src/components/ConferencePage/ConferencePage.js
+++ frontend/src/components/ConferencePage/ConferencePage.js
@@ -44,7 +44,7 @@ export default class ConferencePage extends Component {
                     </div>
                     <div className={"text_right"}>
                         <p className={"color_white font_size_very_big uppercase"}>Банковское сопровождение контактов</p>
-                        <p className={"color_pink font_size_big uppercase"}>Часовой пояс: Москва, Россия (UTC+3)</p>
+                        <p className={"color_pink font_size_big uppercase"}>Часовой пояс: Баку, Азербайджан (UTC+4)</p>
                     </div>
                 </div>
                 <div className={"padding_side"}>
