3de163c4891a0ebd8e23cfcf4ae91f2c292a1292
timager <timger98@gmail.com>
2020-05-18 18:25:31 +0300

    reply to element

--- frontend/src/components/ConferencePage/Chat/MessageSender/MessageSender.js
+++ frontend/src/components/ConferencePage/Chat/MessageSender/MessageSender.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCommentDots} from "@fortawesome/free-solid-svg-icons";
+import {faCommentDots, faTimes} from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 import {API, AXIOS_CONFIG} from "../../../../const/const";
 import "./MessageSender.css";
@@ -16,8 +16,8 @@ export default class MessageSender extends Component {
         }
     }
 
-    send(){
-        if(this.state.message !== ""){
+    send() {
+        if (this.state.message !== "") {
             axios.post(API.CHAT_MESSAGE(this.props.chatId),
                 {text: this.state.message}, AXIOS_CONFIG).then(
                 res => {
@@ -29,15 +29,34 @@ export default class MessageSender extends Component {
         }
     }
 
+    getReplyElement() {
+        if (this.props.reply) {
+            let text = "Ответ для " + this.props.reply.user.name + " " + this.props.reply.user.surname;
+            return (
+                <>
+                    {text} <FontAwesomeIcon className={"cancel_reply"} icon={faTimes}/>
+                </>
+            );
+        } else {
+            return (
+                <>Поделитесь своим мнением</>
+            )
+        }
+    }
+
     render() {
         return (
             <div className={"send_message_container"}>
-                {this.props.reply && (this.props.reply.user.name + ": " + this.props.reply.text)}
+                <div>
+                    {this.getReplyElement()}
+                </div>
                 <TextareaAutosize
                     maxRows={10}
                     className={"textarea"}
                     value={this.state.message}
-                    onChange={(e)=>{this.setState({message:e.target.value});}}
+                    onChange={(e) => {
+                        this.setState({message: e.target.value});
+                    }}
                     placeholder={"Введите сообщение"}/>
                 <FontAwesomeIcon onClick={this.send.bind(this)} className={"send_button"} icon={faCommentDots}/>
             </div>
