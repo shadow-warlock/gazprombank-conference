a43d0c783b27590c288141c1d831a92810decb47
timager <timger98@gmail.com>
2020-05-22 21:04:55 +0300

    migration to new poll structure, may be useless

--- src/Migrations/Version20200522152624.php
+++ src/Migrations/Version20200522152624.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200522141302 extends AbstractMigration
+final class Version20200522152624 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,11 +22,10 @@ final class Version20200522141302 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE poll ADD name VARCHAR(255) NOT NULL, DROP question, DROP variants');
-        $this->addSql('ALTER TABLE answer DROP FOREIGN KEY FK_DADD4A253C947C0F');
-        $this->addSql('DROP INDEX IDX_DADD4A253C947C0F ON answer');
-        $this->addSql('DROP INDEX answer_unique ON answer');
-        $this->addSql('ALTER TABLE answer CHANGE poll_id question_id INT NOT NULL');
+        $this->addSql('CREATE TABLE conference (id INT AUTO_INCREMENT NOT NULL, poll_id INT DEFAULT NULL, chat_id INT NOT NULL, url VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_911533C83C947C0F (poll_id), UNIQUE INDEX UNIQ_911533C81A9A7125 (chat_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE poll (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE conference ADD CONSTRAINT FK_911533C83C947C0F FOREIGN KEY (poll_id) REFERENCES poll (id)');
+        $this->addSql('ALTER TABLE conference ADD CONSTRAINT FK_911533C81A9A7125 FOREIGN KEY (chat_id) REFERENCES chat (id)');
         $this->addSql('ALTER TABLE answer ADD CONSTRAINT FK_DADD4A251E27F6BF FOREIGN KEY (question_id) REFERENCES question (id)');
         $this->addSql('CREATE INDEX IDX_DADD4A251E27F6BF ON answer (question_id)');
         $this->addSql('CREATE UNIQUE INDEX answer_unique ON answer (user_id, question_id)');
@@ -40,15 +39,13 @@ final class Version20200522141302 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE conference DROP FOREIGN KEY FK_911533C83C947C0F');
+        $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494E3C947C0F');
+        $this->addSql('DROP TABLE conference');
+        $this->addSql('DROP TABLE poll');
         $this->addSql('ALTER TABLE answer DROP FOREIGN KEY FK_DADD4A251E27F6BF');
         $this->addSql('DROP INDEX IDX_DADD4A251E27F6BF ON answer');
         $this->addSql('DROP INDEX answer_unique ON answer');
-        $this->addSql('ALTER TABLE answer CHANGE question_id poll_id INT NOT NULL');
-        $this->addSql('ALTER TABLE answer ADD CONSTRAINT FK_DADD4A253C947C0F FOREIGN KEY (poll_id) REFERENCES poll (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
-        $this->addSql('CREATE INDEX IDX_DADD4A253C947C0F ON answer (poll_id)');
-        $this->addSql('CREATE UNIQUE INDEX answer_unique ON answer (user_id, poll_id)');
-        $this->addSql('ALTER TABLE poll ADD question LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, ADD variants LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:array)\', DROP name');
-        $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494E3C947C0F');
         $this->addSql('DROP INDEX IDX_B6F7494E3C947C0F ON question');
         $this->addSql('ALTER TABLE question ADD name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, DROP poll_id, DROP question, DROP variants');
     }
