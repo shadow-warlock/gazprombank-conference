47376754054c4843aa9859e2e288eea0e62b1b13
Kelebrimbor <egor_anikin_n@mail.ru>
2020-05-22 21:31:16 +0300

    Front Update Poll

---
+++ frontend/src/components/AdminPage/ChangePoll/AddPollForm/Question.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Input from "../../../Input/Input";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTimes} from "@fortawesome/free-solid-svg-icons";
+import Button from "../../../Button/Button";
+
+class Question extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <div className={"form_container"}>
+                    <p className={"color_white"}>Вопрос: </p>
+                    <Input value={this.props.question.name} onChange={(e) => {
+                        this.props.inputName(this.props.index, e.target.value);
+                    }}/>
+                </div>
+                <div className={"form_container pb_20"}>
+                    <p className={"color_white"}>{this.props.question.variants.length > 0 ? "Варианты ответа: " : "Без вариантов ответа"}</p>
+                    <div
+                        className={"question_answers_container"}>{this.props.question.variants.map((variants, index) => {
+                        return <div key={index} className={"not_use_this_please"}>
+                            <Input
+                                value={variants}
+                                onChange={(e) => {
+
+                                    this.props.inputVariant(this.props.index, index, e.target.value);
+                                }}
+                            />
+                            <FontAwesomeIcon
+                                onClick={() => {
+                                    this.props.deleteVariant(this.props.index, index)
+                                }}
+                                className={"remove_answer_button"}
+                                icon={faTimes}/>
+                        </div>
+                    })}
+                        <Button
+                            onClick={() => {
+                                this.props.addVariant(this.props.index);
+                            }}>
+                            Добавить ответ
+                        </Button></div>
+                </div>
+                <div>
+                    <Button onClick={() => {
+                        this.props.deleteQuestion(this.props.index);
+                    }}>
+                        удалить вопрос
+                    </Button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Question;
