ac6dcda7c7c1543f648326562b72ea9d5d9fcf7e
timager <timger98@gmail.com>
2020-07-25 21:41:37 +0300

    information partners logos position

--- frontend/src/components/ConferencePage/Footer/Partners/Partners.js
+++ frontend/src/components/ConferencePage/Footer/Partners/Partners.js
@@ -11,31 +11,28 @@ import LogiRus from "../../../../assets/cordiant_images/LogiRus.png"
 import logistics from "../../../../assets/cordiant_images/logistics.png"
 import RAMR from "../../../../assets/cordiant_images/RAMR.png"
 import trucks_on_the_road from "../../../../assets/cordiant_images/trucks_on_the_road.png"
+import {FormattedMessage} from "react-intl";
 
 
 class Partners extends React.Component {
     render() {
         return (
-            <div className={"footer_logo"}>
-                <div>
-                    <p>Информационные партнеры</p>
+            <div className={"footer_logo padding_side"}>
+                <div className={"text_center color_blue font_size_very_big"}>
+                    <p><FormattedMessage id={"information_partners"}/></p>
                 </div>
-                <div className={"bottom_rows_logo"}>
-                    <img id={"logo_partners"} src={trucks_on_the_road} alt={"img"}/>
-                    <img id={"logo_partners"} src={ati_su} alt={"img"}/>
-                    <img id={"logo_partners"} src={auto_review} alt={"img"}/>
-                    <img id={"logo_partners"} src={brother} alt={"img"}/>
-                    <img id={"logo_partners"} src={RAMR} alt={"img"}/>
-                </div>
-                <div className={"bottom_rows_logo"}>
-                    <img id={"logo_partners"} src={business_navigator} alt={"img"}/>
-                    <img id={"logo_partners"} src={LogiRus} alt={"img"}/>
-                    <img id={"logo_partners"} src={colesaru} alt={"img"}/>
-                    <img id={"logo_partners"} src={logistics} alt={"img"}/>
-                    <img id={"logo_partners"} src={fifth_wheel} alt={"img"}/>
-                </div>
-                <div id={"logo_partners"} className={"bottom_rows_logo"}>
-                    <img src={flight} alt={"img"}/>
+                <div className={"logos"}>
+                    <img className={"partner_logo"} src={trucks_on_the_road} alt={"partner logo"}/>
+                    <img className={"partner_logo"} src={ati_su} alt={"partner logo"}/>
+                    <img className={"partner_logo"} src={auto_review} alt={"partner logo"}/>
+                    <img className={"partner_logo"} src={brother} alt={"partner logo"}/>
+                    <img className={"partner_logo"} src={RAMR} alt={"partner logo"}/>
+                    <img className={"partner_logo"} src={business_navigator} alt={"partner logo"}/>
+                    <img className={"partner_logo"} src={LogiRus} alt={"partner logo"}/>
+                    <img className={"partner_logo"} src={colesaru} alt={"partner logo"}/>
+                    <img className={"partner_logo"} src={logistics} alt={"partner logo"}/>
+                    <img className={"partner_logo"} src={fifth_wheel} alt={"partner logo"}/>
+                    <img className={"partner_logo"} src={flight} alt={"partner logo"}/>
                 </div>
             </div>
         );
