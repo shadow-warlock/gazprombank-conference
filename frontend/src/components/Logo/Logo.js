36cd5437cbe824057af617be9297a74c1fc92a96
timager <timger98@gmail.com>
2020-07-26 16:46:01 +0300

    change logo component

--- frontend/src/components/Logo/Logo.js
+++ frontend/src/components/Logo/Logo.js
@@ -1,12 +1,14 @@
 import React, {Component} from "react";
-import logo from "../../assets/cordiant/logo color.png";
+import logo_blue from "../../assets/cordiant/logo_blue.png";
+import logo from "../../assets/cordiant/logo.png";
 import "./Logo.css";
 
 export default class Logo extends Component {
     render() {
         return (
             <div className={"logo"}>
-                <img src={logo} alt={"logo_bank"}/>
+                <img src={logo_blue} alt={"cordiant logo"}/>
+                <img src={logo} alt={"cordiant logo"}/>
             </div>
         );
     }
