13bf8c71eb6d8618def1514fdf9e76ddb3aba948
timager <timger98@gmail.com>
2020-09-26 21:11:10 +0300

    ws admin menu design

--- frontend/src/components/AdminPage/ChatServerMenu/ChatServerMenu.js
+++ frontend/src/components/AdminPage/ChatServerMenu/ChatServerMenu.js
@@ -16,50 +16,51 @@ export default class AdminPage extends Component {
         return (
             <div>
                 <div>
+                    <Button onClick={() => {
+                        let self = this;
+                        axios.get(API.WS_SERVER_START, AXIOS_CONFIG).then(
+                            res => {
+                                self.setState({
+                                    log: res.data
+                                });
+                            }
+                        ).catch(e => {
+                            console.error(e);
+                        });
+                    }}>
+                        Запустить
+                    </Button>
+                    <Button onClick={() => {
+                        let self = this;
+                        axios.get(API.WS_SERVER_STOP, AXIOS_CONFIG).then(
+                            res => {
+                                self.setState({
+                                    log: res.data
+                                });
+                            }
+                        ).catch(e => {
+                            console.error(e);
+                        });
+                    }}>
+                        Остановить
+                    </Button>
+                    <Button onClick={() => {
+                        let self = this;
+                        axios.get(API.WS_SERVER_STATUS, AXIOS_CONFIG).then(
+                            res => {
+                                self.setState({
+                                    log: res.data
+                                });
+                            }
+                        ).catch(e => {
+                            console.error(e);
+                        });
+                    }}>
+                        Статус
+                    </Button></div>
+                <div>
                     {this.state.log}
                 </div>
-                <Button onClick={()=>{
-                    let self = this;
-                  axios.get(API.WS_SERVER_START, AXIOS_CONFIG).then(
-                      res => {
-                          self.setState({
-                              log: res.data
-                          });
-                      }
-                  ).catch(e => {
-                      console.error(e);
-                  });
-                }}>
-                  Запустить
-                </Button>
-                <Button onClick={()=>{
-                    let self = this;
-                    axios.get(API.WS_SERVER_STOP, AXIOS_CONFIG).then(
-                        res => {
-                            self.setState({
-                                log: res.data
-                            });
-                        }
-                    ).catch(e => {
-                        console.error(e);
-                    });
-                }}>
-                    Остановить
-                </Button>
-                <Button onClick={()=>{
-                    let self = this;
-                    axios.get(API.WS_SERVER_STATUS, AXIOS_CONFIG).then(
-                        res => {
-                            self.setState({
-                                log: res.data
-                            });
-                        }
-                    ).catch(e => {
-                        console.error(e);
-                    });
-                }}>
-                    Статус
-                </Button>
             </div>
         );
     }
