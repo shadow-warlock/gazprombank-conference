363b72a1d87e800ca84aa541561cd80c6c9fc03b
Nikita Bruqus <bruqusg@gmail.com>
2020-10-30 23:49:44 +0500

    feat: added Header component

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Logo from '../Logo/Logo';
+import ChangeLocaleButton from '../ChangeLocaleButton/ChangeLocaleButton';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header__content">
+                <div className="header__logo">
+                    <Logo />
+                </div>
+                <div className="header__lang-switcher">
+                    <ChangeLocaleButton />
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
