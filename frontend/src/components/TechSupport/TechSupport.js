f25348bf5e94692f1c1e125421914110ff19bd9a
Nikita Bruqus <bruqusg@gmail.com>
2020-10-30 22:27:48 +0500

    refactor: changed TechSupport component styles

--- frontend/src/components/TechSupport/TechSupport.js
+++ frontend/src/components/TechSupport/TechSupport.js
@@ -6,32 +6,26 @@ import {FormattedMessage} from "react-intl";
 export default class TechSupport extends Component {
     render() {
         return (
-            <div className={"tech_support"}>
-                <div>
-                    <p className={"bold uppercase"}>
-                        <FormattedMessage id={"technical_support"}/>
-                    </p>
-                    <div className={"login_supporters"}>
-                        {TECH_SUPPORTERS.map(
-                            (techSupporter) =>
-                                techSupporter.use.includes(this.props.use) ?
-                                    <div key={techSupporter.phone} className={"supporter"}>
-                                        <p className={"bold"}>
-                                            {this.props.role ? <><FormattedMessage id={techSupporter.role}/>{": "}</> : ""}
-                                            <FormattedMessage id={techSupporter.name}/>
-                                        </p>
-                                        <div className={"supporter_contacts"}>
-                                            <p>
-                                                <a href={"tel:" + techSupporter.phone}>{techSupporter.phone}</a>
-                                            </p>
-                                            <p className={"mail"}>
-                                                <a href={"mailto:" + techSupporter.email}>{techSupporter.email}</a>
-                                            </p>
-                                        </div>
-                                    </div>
-                                : ""
-                        )}
-                    </div>
+            <div className="tech-support">
+                <h3 className="tech-support__title"><FormattedMessage id={"technical_support"} /></h3>
+                <div className={"tech-support__supporters"}>
+                    {TECH_SUPPORTERS.map(
+                        (techSupporter) =>
+                            techSupporter.use.includes(this.props.use) ?
+                                <ul key={techSupporter.phone} className="tech-support__support">
+                                    <li className="tech-support__item">
+                                        {this.props.role ? <><FormattedMessage id={techSupporter.role}/>{": "}</> : ""}
+                                        <FormattedMessage id={techSupporter.name}/>
+                                    </li>
+                                    <li className="tech-support__item">
+                                        <a href={"tel:" + techSupporter.phone}>{techSupporter.phone}</a>
+                                    </li>
+                                    <li className="tech-support__item">
+                                        <a href={"mailto:" + techSupporter.email}>{techSupporter.email}</a>
+                                    </li>
+                                </ul>
+                            : ""
+                    )}
                 </div>
             </div>
         );
