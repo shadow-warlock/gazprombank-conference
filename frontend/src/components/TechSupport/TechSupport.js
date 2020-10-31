432f402e60ecd31a40b19e139f22f846d95ceaf9
Nikita Bruqus <bruqusg@gmail.com>
2020-10-31 15:47:21 +0500

    fix: fixed TechSupport text color on different pages

--- frontend/src/components/TechSupport/TechSupport.js
+++ frontend/src/components/TechSupport/TechSupport.js
@@ -7,20 +7,20 @@ export default class TechSupport extends Component {
     render() {
         return (
             <div className="tech-support">
-                <h3 className="tech-support__title"><FormattedMessage id={"technical_support"} /></h3>
+                <h3 className="tech-support__title auth-white"><FormattedMessage id={"technical_support"} /></h3>
                 <div className={"tech-support__supporters"}>
                     {TECH_SUPPORTERS.map(
                         (techSupporter) =>
                             techSupporter.use.includes(this.props.use) ?
                                 <ul key={techSupporter.phone} className="tech-support__support">
-                                    <li className="tech-support__item">
+                                    <li className="tech-support__item auth-white">
                                         {this.props.role ? <><FormattedMessage id={techSupporter.role}/>{": "}</> : ""}
                                         <FormattedMessage id={techSupporter.name}/>
                                     </li>
                                     <li className="tech-support__item">
-                                        <a href={"tel:" + techSupporter.phone}>{techSupporter.phone}</a>
+                                        <a className="auth-white" href={"tel:" + techSupporter.phone}>{techSupporter.phone}</a>
                                     </li>
-                                    <li className="tech-support__item">
+                                    <li className="tech-support__item auth-white">
                                         <a className="tech-support__mail" href={"mailto:" + techSupporter.email}>{techSupporter.email}</a>
                                     </li>
                                 </ul>
