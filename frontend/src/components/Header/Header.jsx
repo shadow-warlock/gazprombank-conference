3f748aa5a57714db9154564a8ff8487b70101354
Nikita Bruqus <bruqusg@gmail.com>
2020-11-05 19:13:48 +0500

    refactor: changed header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 
-import Logo from '../Logo/Logo';
-import ChangeLocaleButton from '../ChangeLocaleButton/ChangeLocaleButton';
+import gazprombankLogo from '../../assets/fin_images/logo_blue.png';
+import wweLogo from '../../assets/fin_images/wwe.png';
+import lathamLogo from '../../assets/fin_images/latham.png';
+import cliffordLogo from '../../assets/fin_images/clifford.png';
 import './Header.css';
 
 const Header = () => {
     return (
         <header className="header">
             <div className="header__content">
-                <div className="header__logo">
-                    <Logo />
-                </div>
-                <div className="header__lang-switcher">
-                    <ChangeLocaleButton />
-                </div>
+                <img
+                    className="header__logo"
+                    src={gazprombankLogo}
+                    alt="gazprombank logo"
+                />
+                <img
+                    className="header__logo"
+                    src={wweLogo}
+                    alt="gazprombank logo"
+                />
+                <img
+                    className="header__logo"
+                    src={lathamLogo}
+                    alt="gazprombank logo"
+                />
+                <img
+                    className="header__logo"
+                    src={cliffordLogo}
+                    alt="gazprombank logo"
+                />
             </div>
         </header>
     );
