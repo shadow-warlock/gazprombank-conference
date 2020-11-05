ac82d495f56bed2312591770ab0e0305a98fad33
Nikita Bruqus <bruqusg@gmail.com>
2020-11-05 23:28:18 +0500

    refactor: changed AuthPage

--- frontend/src/components/AuthPage/AuthPage.js
+++ frontend/src/components/AuthPage/AuthPage.js
@@ -4,9 +4,9 @@ import './AuthPage.css';
 import { FormattedMessage, injectIntl } from 'react-intl';
 import Moment from 'react-moment';
 import { conferenceDate, conferenceTime, USE } from '../../const/mockData';
-import planeAndTransport from '../../assets/plane_and_transport.png';
 import TechSupport from '../TechSupport/TechSupport';
 import Header from '../Header/Header';
+import Contacts from '../Contacts/Contacts';
 
 class AuthPage extends Component {
     render() {
@@ -14,48 +14,42 @@ class AuthPage extends Component {
             <div className="auth-page">
                 <Header />
                 <section className="auth-page__content">
-                    <div className="auth-page__image-side">
-                        <img
-                            className="auth-page__main-img"
-                            src={planeAndTransport}
-                            alt={'plane and transport'}
-                        />
-                        <div className="auth-page__left-triangles">
-                            <span className="triangle auth-page__left-triangle"></span>
-                            <span className="triangle auth-page__left-triangle"></span>
+                    <div className="auth-page__main-block">
+                        <div className="auth-page__general">
+                            <h1 className="auth-page__title">
+                                <FormattedMessage id={'conference'} />
+                            </h1>
+                            <h1 className="auth-page__conf-theme">
+                                <FormattedMessage id={'conference_theme'} />
+                            </h1>
+                            <div className="auth-page__time">
+                                <Moment
+                                    className={'upper'}
+                                    format={this.props.intl.formatMessage({
+                                        id: 'date_format',
+                                    })}
+                                    date={conferenceDate}
+                                />
+                                {', '}
+                                {conferenceTime} (GMT+3,{' '}
+                                <FormattedMessage id={'moscow'} />)
+                            </div>
+                            <div className="auth-page__form">
+                                <AuthForm setUser={this.props.setUser} />
+                            </div>
                         </div>
                     </div>
-                    <div className="auth-page__info-side">
-                        <h1 className="auth-page__title">
-                            <FormattedMessage id={'conference'} />
-                        </h1>
-                        <h1 className="auth-page__conf-theme">
-                            <FormattedMessage id={'conference_theme'} />
-                        </h1>
-                        <span className="auth-page__time">
-                            <Moment
-                                className={'upper'}
-                                format={this.props.intl.formatMessage({
-                                    id: 'date_format',
-                                })}
-                                date={conferenceDate}
-                            />
-                            {', '}
-                            {conferenceTime} (GMT+3,{' '}
-                            <FormattedMessage id={'moscow'} />)
-                        </span>
-                        <div className="auth-page__form">
-                            <AuthForm setUser={this.props.setUser} />
+                    <footer className="auth-page__tech-block">
+                        <div className="auth-page__tech-content">
+                            <div className="auth-page__contacts">
+                                <Contacts role={true} use={USE.AUTH} />
+                            </div>
+                            <div className="auth-page__tech-support">
+                                <TechSupport role={false} use={USE.AUTH} />
+                            </div>
                         </div>
-                    </div>
+                    </footer>
                 </section>
-                <footer className="auth-page__footer">
-                    <TechSupport role={false} use={USE.AUTH} />
-                    <div className="auth-page__bottom-triangles">
-                        <span className="small-triangle auth-page__bottom-triangle"></span>
-                        <span className="small-triangle auth-page__bottom-triangle"></span>
-                    </div>
-                </footer>
             </div>
         );
     }
