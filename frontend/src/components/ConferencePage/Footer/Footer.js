d475072fbfbe02e7424dd17c7f328e050fc08471
Nikita Bruqus <bruqusg@gmail.com>
2020-11-06 21:50:10 +0500

    fix: changed ConferencePage

--- frontend/src/components/ConferencePage/Footer/Footer.js
+++ frontend/src/components/ConferencePage/Footer/Footer.js
@@ -11,32 +11,40 @@ export default class Footer extends Component {
             <div className="footer">
                 <div className="footer__content">
                     <div className="footer__privacy privacy">
-                        <h5 className="privacy__title">© 2020, ATO Events</h5>
-                        <span className="privacy__policy-row">
-                            <a
-                                target={'_blank'}
-                                href={
-                                    'https://events.ato.ru/upload/FILES/privacy_policy_ru.pdf'
-                                }
-                            >
-                                <FormattedMessage id={'privacy_policy'} />
-                            </a>
-                            <a
-                                target={'_blank'}
-                                href={
-                                    'https://drive.google.com/file/d/1NIkpg7p6aCoaIuyqJ7-RbU928AxOt34R/view'
-                                }
-                            >
-                                <FormattedMessage id={'personal_data_policy'} />
-                            </a>
+                        <span className="privacy__title">
+                            © 2020, ATO Events
                         </span>
+                        <a
+                            className="privacy__policy privacy__link"
+                            target={'_blank'}
+                            href={
+                                'https://events.ato.ru/upload/FILES/privacy_policy_ru.pdf'
+                            }
+                        >
+                            <FormattedMessage id={'privacy_policy'} />
+                        </a>
+                        <a
+                            className="privacy__data privacy__link"
+                            target={'_blank'}
+                            href={
+                                'https://drive.google.com/file/d/1NIkpg7p6aCoaIuyqJ7-RbU928AxOt34R/view'
+                            }
+                        >
+                            <FormattedMessage id={'personal_data_policy'} />
+                        </a>
                         <span className="privacy__contacts">
                             <FormattedMessage id={'contacts'} />
-                            <a href={'tel:+7 (495) 108-5143'}>
-                                {' '}
-                                {': +7 (495) 108-5143'}
-                            </a>
+                            {': '}
                             <a
+                                className="privacy__link"
+                                href={'tel:+7 (495) 108-5143'}
+                            >
+                                {'+7 (495) 108-5143'}
+                            </a>
+                        </span>
+                        <span className="privacy__media">
+                            <a
+                                className="privacy__link"
                                 target={'_blank'}
                                 href={
                                     'https://www.facebook.com/ATOEvents?fref=ts'
@@ -45,6 +53,7 @@ export default class Footer extends Component {
                                 Facebook
                             </a>
                             <a
+                                className="privacy__link"
                                 target={'_blank'}
                                 href={
                                     'https://www.youtube.com/user/eventsatoru'
