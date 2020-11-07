ba248acda88ff7275ac16fb7444961bfac850936
nik <nik_mak@bk.ru>
2020-11-07 18:33:23 +0300

    add rotation on test page

--- frontend/src/components/Router/Router.js
+++ frontend/src/components/Router/Router.js
@@ -8,6 +8,7 @@ import "./../App.css";
 import AdminPage from "../AdminPage/AdminPage";
 import "./../mobile.css";
 import RoomPage from "../RoomPage/RoomPage";
+import Rotation from "react-rotation";
 
 class Router extends Component{
 
@@ -31,6 +32,14 @@ class Router extends Component{
                         <Security roles={[ROLE.ADMIN]}>
                             {user => <AdminPage/>}
                         </Security>
+                    </Route>
+                    <Route exact path={"/test"}>
+                        <Rotation autoPlay={2000} cycle={true}>
+                            <img className={'frame'} src='/assets/speakers/grishin.jpg' />
+                            <img className={'frame'} src='/assets/speakers/kamishev.jpg' />
+                            <img className={'frame'} src='/assets/speakers/lopatnikova.jpg' />
+                            <img className={'frame'} src='/assets/speakers/melnikov.jpg' />
+                        </Rotation>
                     </Route>
                 </Switch>
             </BrowserRouter>
