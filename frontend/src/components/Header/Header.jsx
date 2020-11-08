697579f4165f12e2a811475a90cf73031417faba
Nikita Bruqus <bruqusg@gmail.com>
2020-11-08 20:55:13 +0500

    fix: fixed header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,32 +4,51 @@ import gazprombankLogo from '../../assets/fin_images/gazprom.png';
 import wweLogo from '../../assets/fin_images/wwe.png';
 import lathamLogo from '../../assets/fin_images/latham.png';
 import cliffordLogo from '../../assets/fin_images/clifford.png';
+import freshfieldsLogo from '../../assets/fin_images/freshfields.png';
 import './Header.css';
 
 const Header = () => {
     return (
         <header className="header">
             <div className="header__content">
-                <img
-                    className="header__logo"
-                    src={gazprombankLogo}
-                    alt="gazprombank logo"
-                />
-                <img
-                    className="header__logo"
-                    src={wweLogo}
-                    alt="gazprombank logo"
-                />
-                <img
-                    className="header__logo"
-                    src={lathamLogo}
-                    alt="gazprombank logo"
-                />
-                <img
-                    className="header__logo"
-                    src={cliffordLogo}
-                    alt="gazprombank logo"
-                />
+                <a href="https://www.gazprombank.ru/" target="_blank">
+                    <img
+                        className="header__logo"
+                        src={gazprombankLogo}
+                        alt="gazprombank logo"
+                    />
+                </a>
+                <a href="https://wwe-conferences.com/" target="_blank">
+                    <img
+                        className="header__logo"
+                        src={wweLogo}
+                        alt="wwe-conferences logo"
+                    />
+                </a>
+                <a href="https://www.lw.com/" target="_blank">
+                    <img
+                        className="header__logo"
+                        src={lathamLogo}
+                        alt="latham logo"
+                    />
+                </a>
+                <a
+                    href="https://www.cliffordchance.com/people_and_places/places/europe/russia.html"
+                    target="_blank"
+                >
+                    <img
+                        className="header__logo"
+                        src={cliffordLogo}
+                        alt="clifford logo"
+                    />
+                </a>
+                <a href="https://www.freshfields.com/" target="_blank">
+                    <img
+                        className="header__logo"
+                        src={freshfieldsLogo}
+                        alt="freshfields logo"
+                    />
+                </a>
             </div>
         </header>
     );
